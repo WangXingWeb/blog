ce32e07455ab8861c3fc2a99449d4da0ccbdcd0c
王星 <wx1620@163.com>
2018-06-28 18:00:17 +0800

    工具方法util

--- comp/util.js
+++ comp/util.js
@@ -1,22 +1,25 @@
 /**
- * Created by 王星 on 2018/6/19.
+ * @author 王星 'www.iwangxing.cn'
+ * @description js常用的工具类方法集合
+ * @version 0.1
  */
+
+/**************************************/
+
 /**
- * 日期格式化
- * @return string
+ * @description 日期格式化
+ * @param {data} or {string} data 需要格式化的日期或者日期字符串
+ * @param {string} format 模板  @example:'yyyy-MM-dd hh:mm'
+ * @return {string}
  */
 var dateFormat = function(date, format){
-
     if(String(date).indexOf("-")>=0){
-        date=String(date).replace(/-/g,"/");
+        date=String(date).replace(/-/g, "/");
     }
-
     if(String(date).indexOf(".")>=0){
         date=date*1000;
     }
-
     date = new Date(date);
-
     var map = {
         "M": date.getMonth() + 1, //月份
         "d": date.getDate(), //日
@@ -42,3 +45,29 @@ var dateFormat = function(date, format){
     });
     return format;
 };
+/**
+ * @description 获取文本中的图片
+ * @param {string} html html字符串
+ * @return {array} imgs 图片地址的数组
+ */
+var pickupImg=function (html) {
+    var imgs=[];
+    html.replace(/<img\ssrc=[\'"](.+?)[\'"]\>/gi, function (match, capture) {
+        imgs.push(capture);                                                                        // match 匹配的img标签
+    });                                                                                            // capture img中的src
+    return imgs;
+}
+
+/**
+ * @description 获取文本中的文字
+ * @param {string} strs html字符串
+ * @param {string} length 需要截取的文本长度
+ * @return {string} result 截取的文字
+ */
+var pickupText=function (strs,length) {
+    //过滤html
+    var str=strs.replace(/<[^>]+>/g,"");
+    var result=str.substr(0,length);
+    console.log(result);
+    return result;
+}
