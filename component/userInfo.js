39d06dda0b1f55d314ee8da01b861a5e51ada20a
王星 <wx1620@163.com>
2018-07-24 18:02:02 +0800

    组件化拆分

---
+++ component/userInfo.js
@@ -0,0 +1,42 @@
+//用户信息及写作时间组件
+Vue.component('userinfo', {
+    data: function () {
+        return {
+            user: {
+                attributes:{
+                    username:"",
+                    headImg:''
+                }
+            },
+            createdTime:''
+        }
+    },
+    props:['userid','created'],
+    watch:{
+        userid:function (newVal,oldVal) {
+            var _this=this;
+            mainBmob.getSingleData('_User',newVal).then(function (data) {
+                console.log(data);
+                if(data.code==200){
+                    _this.user=data.result;
+                }else{
+                    _this.$messagebox('获取数据错误', '请确认您的网络是否通畅');
+                }
+            });
+        },
+        created:function (newVal,oldVal) {
+            this.createdTime=dateFormat(newVal,'yyyy.MM.dd hh:mm');
+        }
+    },
+    template:
+    '<div class="userInfo">'+
+    '<div class="headImg-container">'+
+    '<img class="head-img" style="" :src="user.attributes.headImg" alt="">'+
+    '</div>'+
+    '<div class="author-name">{{ user.attributes.username }}</div>'+
+    '<div class="created-time">{{ createdTime }}</div>'+
+    '</div>',
+    methods:{
+
+    }
+});
