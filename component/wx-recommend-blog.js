71958bcee3f8776a9966cc25660ef82810a052f4
王星 <wx1620@163.com>
2018-08-01 10:36:01 +0800

    推荐完成

--- component/wx-recommend-blog.js
+++ component/wx-recommend-blog.js
@@ -1,13 +1,7 @@
 //用户信息及写作时间组件
-Vue.component('wx-like-blog', {
+Vue.component('wx-recommend-blog', {
     data: function () {
         return {
-            user: {
-                attributes:{
-                    username:"",
-                    headImg:''
-                }
-            },
             blog:{
                 attributes:{
                     title:"",
@@ -23,53 +17,32 @@ Vue.component('wx-like-blog', {
     },
     created:function () {
         var _this=this;
-        mainBmob.getSingleData('_User',_this.dynamic.attributes.user).then(function (data) {
+        mainBmob.getSingleData('_User',_this.recommend.attributes.author).then(function (data) {
             if(data.code==200){
-                _this.user=data.result;
-            }else{
-                _this.$messagebox('获取数据错误', '请确认您的网络是否通畅');
-            }
-        });
-        mainBmob.getSingleData('Blog',_this.dynamic.attributes.blog).then(function (data) {
-            if(data.code==200){
-                _this.blog=data.result;
-                return mainBmob.getSingleData('_User',_this.blog.attributes.author);
-            }else{
-                _this.$messagebox('获取数据错误', '请确认您的网络是否通畅');
-            }
-        }).then(function (data) {
-            if(data.code==200){
-                console.log(data.result);
                 _this.author=data.result;
             }else{
                 _this.$messagebox('获取数据错误', '请确认您的网络是否通畅');
             }
         });
     },
-    props:['dynamic'],
+    props:['recommend'],
     template:
-    '<li class="like-blog" @click="readBlog(blog.id)"><div class="like-header"><div class="like-headImg-container">'+
-    '<img class="like-headImg" :src="user.attributes.headImg" alt="">'+
-    '</div><div class="like-type-name">'+
-    '<div class="like-name">{{user.attributes.username}} </div>'+
-    '<div class="like-type" v-if="dynamic.attributes.type==0">赞了</div><div class="like-type" v-else>发布了</div> 文章 </div>'+
-    '<div class="like-time" v-html="resetTime(dynamic.createdAt)"></div> </div>'+
-    '<div class="like-content">'+
-    '<div class="like-title">{{ blog.attributes.title }}</div>'+
+    '<li class="like-blog" @click="readBlog(recommend.id)"> <div class="like-header"> <div class="like-headImg-container">'+
+    '<img alt="" :src="author.attributes.headImg" class="like-headImg"></div><div class="like-type-name">'+
+    '<div class="like-name">{{author.attributes.username}} </div> </div>'+
+    '<div class="like-time">{{ resetTime(recommend.createdAt) }}</div>'+
+    '</div><div class="like-content">'+
+    '<div class="like-title">{{ recommend.attributes.title }}</div>'+
     '<div class="like-content-main">'+
-    '<div class="like-info">{{getContent(blog.attributes.content)}}</div>'+
-    '<div class="like-content-img" :style="{background: \'url(\'+ getImg(blog.attributes.content) +\')\'}"></div>'+
+    '<div class="like-info">{{getContent(recommend.attributes.content)}}</div>'+
+    '<div class="like-content-img" :style="{background: \'url(\'+ getImg(recommend.attributes.content) +\')\'}"></div>'+
     '</div> </div> <div class="like-footer">'+
-    '<div class="like-footer-left">'+
-    '<div class="like-author-name">{{ author.attributes.username }}</div>'+
-    '<div class="like-num">'+
-    '<span>{{ blog.attributes.commentNum }}</span>'+
-    '<i class="icon iconfont icon-dianzan1"></i> </div>'+
-    '<div class="like-num">'+
-    '<span >{{ blog.attributes.applaudNum }}</span> <i class="icon iconfont icon-pinglun"></i>'+
-    '</div></div>'+
-    '<i class="icon iconfont icon-gengduo"></i>'+
-    '</div> </li>',
+    '<div class="like-num recommend-num">'+
+    '<span>{{ recommend.attributes.commentNum }}</span>'+
+    '<i class="icon iconfont icon-dianzan1"></i>'+
+    '</div> <div class="like-num recommend-num">'+
+    '<span>{{ recommend.attributes.applaudNum }}</span> <i class="icon iconfont icon-pinglun"></i>'+
+    '</div> </div> </li>',
     methods:{
         getContent:function (str) {
             return pickupText(str,100);
