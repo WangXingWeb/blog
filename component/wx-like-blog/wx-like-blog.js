fda7d585e1981ef854a63fd86a2797749e6a5132
王星 <wx1620@163.com>
2018-08-20 10:42:51 +0800

    修复动态中收藏和发布错误的bug

--- component/wx-like-blog/wx-like-blog.js
+++ component/wx-like-blog/wx-like-blog.js
@@ -53,7 +53,7 @@ Vue.component('wx-like-blog', {
     '</div><div class="like-type-name">'+
     '<div class="like-name">{{user.attributes.username}} </div>'+
     '<div class="like-type" v-if="dynamic.attributes.type==0">赞了</div>' +
-    '<div class="like-type" v-if="dynamic.attributes.type==1">发布了</div>' +
+    '<div class="like-type" v-else-if="dynamic.attributes.type==1">发布了</div>' +
     '<div class="like-type" v-else>收藏了</div> 文章 </div>'+
     '<div class="like-time" v-html="resetTime(dynamic.createdAt)"></div> </div>'+
     '<div class="like-content">'+
